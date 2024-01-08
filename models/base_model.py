fc52cfb2378f9b39574e8abc8794710fc72e39d2
glenmiracle18 <m.bonyu@alustudent.com>
2024-01-08 21:10:37 +0200

    build(structure): creating the file structure

---
+++ models/base_model.py
@@ -0,0 +1,26 @@
+#!usr/bin/python3
+# base model to for children to inherit
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        
+        # formatting the string
+        def __str__(self):
+            return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        # save for main model
+        def save(self):
+            self.updated_at = datetime.now()
+
+        #converts the object to a dictionary
+        def to_dict(self):
+            obj_dict = self.__dict__.copy()
+            obj_dict['__class__'] = self.__class__.__name__
+            obj_dict['created_at'] = self.created_at.isoformat()
+            obj_dict['updated_at'] = self.updated_at.isoformat()
+            return obj_dict;
