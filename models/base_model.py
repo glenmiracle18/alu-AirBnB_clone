64a094bccdd8d43edc8af18cbf104716d740aec9
glenmiracle18 <m.bonyu@alustudent.com>
2024-01-08 21:14:06 +0200

    build(basemodel): built and tested the first base model function

--- models/base_model.py
+++ models/base_model.py
@@ -10,17 +10,17 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         
-        # formatting the string
-        def __str__(self):
-            return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-        # save for main model
-        def save(self):
-            self.updated_at = datetime.now()
+    # formatting the string
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    # save for main model
+    def save(self):
+        self.updated_at = datetime.now()
 
-        #converts the object to a dictionary
-        def to_dict(self):
-            obj_dict = self.__dict__.copy()
-            obj_dict['__class__'] = self.__class__.__name__
-            obj_dict['created_at'] = self.created_at.isoformat()
-            obj_dict['updated_at'] = self.updated_at.isoformat()
-            return obj_dict;
+    #converts the object to a dictionary
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict;
