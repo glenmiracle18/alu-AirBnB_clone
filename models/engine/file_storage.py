3ae07d98d1ae2c8b731d8578d894cc0a93a41780
glenmiracle18 <m.bonyu@alustudent.com>
2024-01-09 20:55:18 +0200

    build(file_storage): building file_storage functionalities

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,21 +1,13 @@
-
 #!/usr/bin/python3
 """Defines the FileStorage class."""
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
 
 
 class FileStorage:
     """Represent an abstracted storage engine.
 
     Attributes:
-        __file_path (str): The name of the file to save objects to.
+        __file_path (str): file path to save the object.
         __objects (dict): A dictionary of instantiated objects.
     """
     __file_path = "file.json"
