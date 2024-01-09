e2a0f8ef5a42633cebe7bb54c8af4ba02dd78bea
glenmiracle18 <m.bonyu@alustudent.com>
2024-01-09 21:28:55 +0200

    build(console.py): building console for the airbnb app

--- console.py
+++ console.py
@@ -1,23 +1,135 @@
 #!/usr/bin/python3
-"""Defines the HBNBCommand class."""
+"""Defines the HBNBCommand"""
 import cmd
+import json
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
+    valid_classes = ["BaseModel"]  # Add more classes as needed
 
-    def do_quit(self, arg):
-        """Quit command to exit the program"""
-        return True
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it, and prints the id"""
+        if not arg:
+            print("** class name missing **")
+            return
 
-    def do_EOF(self, arg):
-        """EOF command to exit the program"""
-        print("")  # Print a newline before exiting
-        return True
+        class_name = arg
+        if class_name not in HBNBCommand.valid_classes:
+            print("** class doesn't exist **")
+            return
 
-    def emptyline(self):
-        """Do nothing on empty line + ENTER"""
-        pass
+        new_instance = BaseModel()
+        new_instance.save()
+        print(new_instance.id)
+
+    def do_show(self, arg):
+        """string representation of instance """
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+        if class_name not in HBNBCommand.valid_classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        obj_id = args[1]
+        key = f"{class_name}.{obj_id}"
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+
+        print(storage.all()[key])
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+        if class_name not in HBNBCommand.valid_classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        obj_id = args[1]
+        key = f"{class_name}.{obj_id}"
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+
+        del storage.all()[key]
+        storage.save()
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances"""
+        args = arg.split()
+        if args and args[0] not in HBNBCommand.valid_classes:
+            print("** class doesn't exist **")
+            return
+
+        instances = []
+        for key, value in storage.all().items():
+            if not args or value.__class__.__name__ == args[0]:
+                instances.append(str(value))
+        print(instances)
+
+    def do_update(self, arg):
+        """Updates the instance based on id"""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+        if class_name not in HBNBCommand.valid_classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        obj_id = args[1]
+        key = f"{class_name}.{obj_id}"
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+
+        attribute_name = args[2]
+        if len(args) < 4:
+            print("** value missing **")
+            return
+
+        attribute_value = args[3]
+
+        # Cast attribute value based on attribute type
+        if hasattr(storage.all()[key], attribute_name):
+            attr_type = type(getattr(storage.all()[key], attribute_name))
+            if attr_type == int:
+                attribute_value = int(attribute_value)
+            elif attr_type == float:
+                attribute_value = float(attribute_value)
+
+        setattr(storage.all()[key], attribute_name, attribute_value)
+        storage.save()
 
 
 if __name__ == '__main__':
