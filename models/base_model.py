32d71ecae3a75292fe88b49fa72003f9f5794b63
glenmiracle18 <m.bonyu@alustudent.com>
2024-01-09 20:22:51 +0200

    build(tests): build test files for my modules

--- models/base_model.py
+++ models/base_model.py
@@ -1,37 +1,52 @@
-#!usr/bin/python3
-# base model to for children to inherit
 
-import uuid
+#!/usr/bin/python3
+"""Defines the BaseModel class."""
+from uuid import uuid4
 from datetime import datetime
+import models
 
 
 class BaseModel:
-    """
-    Base class for all models
-    """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    """Represents the BaseModel of the HBnB project."""
 
-    # formatting the string
-    def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
 
-    # save for main model
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
+
     def save(self):
-        """
-        Saves and updates the state of the base model
-        """
-        self.updated_at = datetime.now()
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.today()
+        models.storage.save()
 
-    # converts the object to a dictionary
     def to_dict(self):
+        """Return the dictionary of the BaseModel instance.
+
+        Includes the key/value pair __class__ representing
+        the class name of the object.
         """
-        Returns a dictionary containing
-        """
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
